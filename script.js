9042aacc1ccbc376a68ac4fdbd72e95344ec993d
Docked <116732014+Dockedchutoy@users.noreply.github.com>
2025-08-21 15:57:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+let score = 0;
+document.getElementById("counter").innerHTML = score;
+let upgrade = 1;
+let auto_upgrade = 0;
+let ac_interval = 1000; // ac = autoclicker
+
+// btn
+btn = document.getElementById("clicker")
+btn.addEventListener("click", function() {
+    score += upgrade;
+    document.getElementById("counter").innerHTML = score;
+});
+
+// Autoclicker upgrade 1
+upgrade1 = document.getElementById("upgrade1")
+upgrade1.addEventListener("click", function() {
+    if( score >= 10){
+        score -=10;
+        if (auto_upgrade == 0) {
+            auto_upgrade += 1;
+            document.getElementById("counter").innerHTML = score;
+        } else {
+            auto_interval -= 2;
+            document.getElementById("counter").innerHTML = score;
+        }
+    }
+
+});
+
+// Manual clicker upgrade 1
+upgrade2 = document.getElementById("upgrade2")
+upgrade2.addEventListener("click", function() {
+    if( score >= 100){
+        score -= 100;
+        upgrade += 1;
+        document.getElementById("counter").innerHTML = score;
+    }
+
+});
+
+// Auto clicker
+setInterval(autoClicker, ac_interval)
+function autoClicker() {
+    score += auto_upgrade;
+    document.getElementById("counter").innerHTML = score;
+}
