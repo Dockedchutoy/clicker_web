7341022d31df20a72b8c9ff74aa48520f2afdd5a
Docked <116732014+Dockedchutoy@users.noreply.github.com>
2025-08-22 13:05:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // variably
 
-let score = 2000; //penízky
+let score = 0; //penízky
 document.getElementById("counter").innerHTML = score;
 let mc_upgrade = 1; // Za jeden manual click
 
@@ -8,12 +8,26 @@ let ac_upgrade = 1; // Za jeden autoclick
 let ac_interval = 0; // autoclicker speed
 let ac_amount = 0; //Počet za jednu sekundu
 
+let total = 0;
+let total_view = document.getElementById("total")
+
+let highscore = 0;
+let highscore_view = document.getElementById("highscore")
+
 // btn
 
 let btn = document.getElementById("clicker");
 btn.addEventListener("click", function() {
     score += mc_upgrade;
     document.getElementById("counter").innerHTML = score;
+
+    total += mc_upgrade;
+    total_view.innerHTML = `Total: ${total}`;
+
+    if (score > highscore) {
+        highscore = score;
+        highscore_view.innerHTML = `Highscore: ${highscore}`;
+    }
 });
 
 // Manual clicker mc_upgrade 1
@@ -34,14 +48,13 @@ upgrade_mc.addEventListener("click", function() {
         upgrade_mc.innerText = "Double Manual Clicks - BOUGHT OUT"; */
 
         upgrade_mc.innerText = `Manuální Klik - ${upgrade_mc_cost}$`;
-        document.getElementById("mc_per_click").innerHTML = `${mc_upgrade}$ za jeden klik`
+        document.getElementById("mc_per_click").innerHTML = `${mc_upgrade}$ za jeden klik`;
     }
 });
 
 // Autoclicker upgrade +0.5
 
 let upgrade1 = document.getElementById("upgrade1");
-let upgrade1_count = 0;
 let upgrade1_cost = 10;
 upgrade1.addEventListener("click", function() {
     if( score >= upgrade1_cost){
@@ -50,13 +63,12 @@ upgrade1.addEventListener("click", function() {
         ac_amount += 0.5;
         ac_interval = 1000 / ac_amount;
 
-        upgrade1_count += 1;
-        if (upgrade1_count < 6) { upgrade1_cost += 50; }
+        upgrade1_cost += 1.5;
 
         document.getElementById("counter").innerHTML = score;
         upgrade1.innerText = `Autoclicker +0.5 - ${upgrade1_cost}$`
         document.getElementById("ac_interval_view").innerHTML = `Autoclick každých ${(ac_interval / 1000).toFixed(4)} sekund`;     
-        document.getElementById("ac_per_click").innerHTML = `${ac_amount}$ za jednu sekundu`
+        document.getElementById("ac_per_click").innerHTML = `${ac_amount}$ za jednu sekundu`;
     }
 
 });
@@ -64,7 +76,6 @@ upgrade1.addEventListener("click", function() {
 // Autoclicker upgrade +1
 
 let upgrade2 = document.getElementById("upgrade2");
-let upgrade2_count = 0;
 let upgrade2_cost = 50;
 upgrade2.addEventListener("click", function() {
     if( score >= upgrade2_cost){
@@ -73,15 +84,72 @@ upgrade2.addEventListener("click", function() {
         ac_amount += 1;
         ac_interval = 1000 / ac_amount;
 
-        upgrade2_count += 1;
-        if (upgrade2_count < 10) { upgrade2_cost += 50; }
+        upgrade2_cost *= 1.2;
+        upgrade2_cost = Math.round(upgrade2_cost);
 
         document.getElementById("counter").innerHTML = score;
         upgrade2.innerText = `Autoclicker +1 - ${upgrade2_cost}$`
         document.getElementById("ac_interval_view").innerHTML = `Autoclick každých ${(ac_interval / 1000).toFixed(4)} sekund`;
-        document.getElementById("ac_per_click").innerHTML = `${ac_amount}$ za jednu sekundu`
+        document.getElementById("ac_per_click").innerHTML = `${ac_amount}$ za jednu sekundu`;
     }
 
+});
+
+// Autoclicker upgrade +5
+
+let upgrade3 = document.getElementById("upgrade3");
+let upgrade3_cost = 50;
+upgrade3.addEventListener("click", function() {
+    if( score >= upgrade3_cost){
+        score -= upgrade3_cost;
+
+        ac_amount += 5;
+        ac_interval = 1000 / ac_amount;
+
+        upgrade3_cost *= 1.4;
+        upgrade3_cost = Math.round(upgrade3_cost);
+
+        document.getElementById("counter").innerHTML = score;
+        upgrade3.innerText = `Autoclicker +5 - ${upgrade3_cost}$`;
+        document.getElementById("ac_interval_view").innerHTML = `Autoclick každých ${(ac_interval / 1000).toFixed(4)} sekund`;
+        document.getElementById("ac_per_click").innerHTML = `${ac_amount}$ za jednu sekundu`;
+    }
+});
+
+// Manual clicker mc_upgrade 2
+let upgrade_mc2 = document.getElementById("upgrade_mc2");
+let upgrade_mc2_cost = 100;
+upgrade_mc2.addEventListener("click", function() {
+    if (score >= upgrade_mc2_cost) {
+        score -= upgrade_mc2_cost;  // opraveno
+
+        mc_upgrade += 5;
+        document.getElementById("counter").innerHTML = score;
+
+        upgrade_mc2_cost *= 1.6;
+        upgrade_mc2_cost = Math.round(upgrade_mc2_cost);  // uložení zaokrouhlení
+
+        upgrade_mc2.innerText = `Manuální Klik +5 - ${upgrade_mc2_cost}$`;
+        document.getElementById("mc_per_click").innerHTML = `${mc_upgrade}$ za jeden klik`;
+    }
+});
+
+// Manual clicker mc_upgrade 3
+let upgrade_mc3 = document.getElementById("upgrade_mc3");
+let upgrade_mc3_cost = 1000;
+upgrade_mc3.addEventListener("click", function() {
+    if (score >= upgrade_mc3_cost) {
+        score -= upgrade_mc3_cost;  // opraveno
+
+        mc_upgrade += 50;
+        document.getElementById("counter").innerHTML = score;
+
+        upgrade_mc3_cost *= 1.6;
+        upgrade_mc3_cost = Math.round(upgrade_mc3_cost);  // uložení zaokrouhlení
+
+        upgrade_mc3.innerText = `Manuální Klik +5 - ${upgrade_mc3_cost}$`;
+        document.getElementById("mc_per_click").innerHTML = `${mc_upgrade}$ za jeden klik`;
+    }
 });
 
 // Auto clicker
@@ -90,6 +158,14 @@ function autoClicker() {
     if (ac_interval > 0) {
     score += ac_upgrade;
     document.getElementById("counter").innerHTML = score;
+
+    total += ac_upgrade;
+    total_view.innerHTML = `Total: ${total}`;
+
+    if (score > highscore) {
+        highscore = score;
+        highscore_view.innerHTML = `Highscore: ${highscore}`;
+    }
     }
     setTimeout(autoClicker, ac_interval);
 }
